6050cbe856528f79ff41d198f188e16a49ac7f0e
Saveen Toor <toorsaveen@gmail.com>
2019-05-28 19:14:12 -0700

    Added styling for schedule page

--- js/screens/Schedule/Schedule.js
+++ js/screens/Schedule/Schedule.js
@@ -1,16 +1,31 @@
 //import liraries
 import React, { Component } from "react";
 import { View, Text, StyleSheet, SectionList } from "react-native";
-
+import moment from "moment";
+import { styles } from "./styles";
 
 // create a component
 const Schedule = ({ data }) => {
-  console.log(data)
+  console.log(data);
   return (
     <View>
       <SectionList
         sections={data}
-        renderItem={({ item }) => <Text>{item.title}</Text>}
+        renderItem={({ item }) => {
+          return (
+            <View>
+              <View style={styles.textSpacing}>
+                <Text style={styles.h1}>{item.title}</Text>
+                <Text style={styles.h2}>{item.location}</Text>
+              </View>
+              <View style={styles.border} />
+            </View>
+          );
+        }}
+        renderSectionHeader={({ section: { title } }) => (
+          <Text style={styles.scheduleText}>{moment(title).format("LT")}</Text>
+        )}
+        keyExtractor={item => item.id}
       />
     </View>
   );
--- js/screens/Schedule/styles.js
+++ js/screens/Schedule/styles.js
@@ -1,6 +1,27 @@
 import { StyleSheet } from "react-native";
+import { Colors, Fonts } from "../../config/styles";
 
 export const styles = StyleSheet.create({
-
-  });
-  
+  h1: {
+    fontSize: 20,
+    fontWeight: "600",
+    fontFamily: Fonts.light
+  },
+  h2: {
+    fontSize: 15,
+    color: Colors.mediumGrey,
+    fontFamily: Fonts.regular
+  },
+  scheduleText: {
+    backgroundColor: Colors.lightGrey,
+    padding: 5,
+    fontWeight: "600"
+  },
+  textSpacing: {
+    margin: 15
+  },
+  border: {
+    borderBottomWidth: 1.5,
+    borderColor: Colors.lightGrey
+  }
+});
