4ac4d00ca06c9168922ae68588243110bc8361b3
Saveen Toor <toorsaveen@gmail.com>
2019-05-28 20:05:43 -0700

    Added content for session

--- js/screens/Sessions/Sessions.js
+++ js/screens/Sessions/Sessions.js
@@ -1,14 +1,39 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
-import { ScrollView } from "react-native-gesture-handler";
+import { View, Text, StyleSheet, Image } from "react-native";
+import { ScrollView, TouchableOpacity } from "react-native-gesture-handler";
+import moment from "moment";
 
 const Sessions = ({ item, navigation }) => {
   return (
     <ScrollView>
       <View>
-        <Text>
-          {item.location}
-        </Text>
+        <Text>{item.location}</Text>
+        <Text>{item.title}</Text>
+        <Text>{moment(item.startTime).format("LT")}</Text>
+      </View>
+      <View>
+        <Text>{item.description}</Text>
+      </View>
+      <View>
+        <Text>Presented by:</Text>
+        <TouchableOpacity
+          onPress={() => {
+            navigation.navigate("Speaker", {
+              name: item.speaker.name,
+              bio: item.speaker.bio,
+              url: item.speaker.url,
+              image: item.speaker.image,
+              id: item.speaker.id
+            });
+          }}
+        >
+          <Image
+            style={{ width: 100, height: 100, borderRadius: 50 }}
+            source={{ uri: item.speaker.image }}
+          />
+          <Text>{item.speaker.name}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity />
       </View>
     </ScrollView>
   );
