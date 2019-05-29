4b2b6a72f93e351391a347a763cc85a589da5ca4
Saveen Toor <toorsaveen@gmail.com>
2019-05-29 11:12:22 -0700

    Added add, remove async functions

--- js/App.js
+++ js/App.js
@@ -2,12 +2,15 @@ import React, { Component } from "react";
 import { ApolloProvider } from "react-apollo";
 import client from "./config/api";
 import RootStackNavigator from "./navigation/RootStackNavigator";
+import Faves, { FavesProvider } from "./context/FavesContext";
 
 export default class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
+        <FavesProvider>
         <RootStackNavigator />
+        </FavesProvider>
       </ApolloProvider>
     );
   }
--- js/context/FavesContext/FavesContext.js
+++ js/context/FavesContext/FavesContext.js
@@ -1,14 +1,65 @@
-import React, { createContext, Component } from 'react';
+import React, { createContext, Component } from "react";
+import { queryFaves, createFav } from "../../config/models";
 
 const FavesContext = React.createContext();
 class FavesProvider extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      faveIds: [],
+      faveIds: []
     };
   }
-  // more code will go here!
+
+  async componentDidMount() {
+    this.getFavedSessionIds();
+  }
+
+  getFavedSessionIds = async () => {
+    try {
+      const faves = await queryFaves();
+      const ids = faves.map(fave => fave[0]);
+      console.log(faves);
+      this.setState({ faveIds });
+    } catch (e) {
+      return false;
+    }
+  };
+
+  addFaveSession = async sessionId => {
+    try {
+      const newFave = await createFav(sessionId);
+      if (newFave) {
+        this.setState({ faveIds: [...this.state.faveIds, newFave.id] });
+      }
+    } catch (e) {
+      return false;
+    }
+  };
+
+  removeFaveSession = async sessionId => {
+    try {
+      await removedFav(sessionId);
+      this.getFavedSessionIds();
+    } catch (e) {
+      return false;
+    }
+  };
+
+  render() {
+    console.log(this.state.faveIds);
+    return (
+      <FavesContext.Provider
+        value={{
+          ...this.state,
+          addFaveSession: this.addFaveSession,
+          removeFaveSession: this.removeFaveSession
+        }}
+      >
+        {this.props.children}
+      </FavesContext.Provider>
+    );
+  }
 }
+
 export { FavesProvider };
 export default FavesContext;
