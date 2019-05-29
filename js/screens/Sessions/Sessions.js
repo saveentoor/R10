c75d9edb55cefa60fe33835f06f2a744fc688a62
Saveen Toor <toorsaveen@gmail.com>
2019-05-28 19:52:02 -0700

    Initialized query for sessions

--- js/screens/Sessions/Sessions.js
+++ js/screens/Sessions/Sessions.js
@@ -1,25 +1,17 @@
-//import liraries
 import React, { Component } from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
 
-// create a component
-const Sessions = () => {
+const Sessions = ({ item, navigation }) => {
   return (
-    <View style={styles.container}>
-      <Text>MyComponent</Text>
-    </View>
+    <ScrollView>
+      <View>
+        <Text>
+          {item.location}
+        </Text>
+      </View>
+    </ScrollView>
   );
 };
 
-// define your styles
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#2c3e50"
-  }
-});
-
-//make this component available to the app
 export default Sessions;
--- js/screens/Sessions/SessionsContainer.js
+++ js/screens/Sessions/SessionsContainer.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
 import Sessions from "./Sessions";
 import gql from "graphql-tag";
+import { Query } from "react-apollo";
 
 class SessionsContainer extends Component {
   static navigationOptions = {
@@ -12,11 +13,33 @@ class SessionsContainer extends Component {
     let itemID = navigation.getParam("id");
     let item = navigation.getParam("item");
     return (
-      <View style={styles.container}>
-        <Text>SessionsContainer</Text>
-      </View>
+      <Query variables={{ id: itemID }} query={GET_ALL_SESSIONS}>
+        {({ loading, data }) => {
+          if (loading || !data) return <ActivityIndicator size="small" />;
+          console.log(data);
+          return (
+            <Sessions
+              itemID={itemID}
+              item={item}
+              navigation={this.props.navigation}
+              data={data.allSpeaker}
+            />
+          );
+        }}
+      </Query>
     );
   }
 }
+const GET_ALL_SESSIONS = gql`
+  query allSessions($id: ID) {
+    allSpeakers(filter: { id: $id }) {
+      id
+      bio
+      image
+      url
+      name
+    }
+  }
+`;
 
 export default SessionsContainer;
