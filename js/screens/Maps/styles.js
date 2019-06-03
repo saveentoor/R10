dcfa32bcd1aae181850577ec9c65650632ead111
Saveen Toor <toorsaveen@gmail.com>
2019-06-03 15:03:04 -0700

    Fixed map text styling

--- js/screens/Maps/styles.js
+++ js/screens/Maps/styles.js
@@ -3,10 +3,11 @@ import { Fonts, Colors } from "../../config/styles";
 
 export const styles = StyleSheet.create({
   maps: {
-    fontSize: 36,
+    fontSize: 26,
     fontWeight: "bold",
     margin: 10,
     fontFamily: Fonts.regular,
-    paddingTop: 200
+    paddingTop: 200,
+    textAlign: "center"
   }
 });
